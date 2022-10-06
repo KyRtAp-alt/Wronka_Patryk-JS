0b7a34e68d45ec3b061245167a8ad57abece6c3e
KyRtAp-alt <patrykwronka65@wp.pl>
2022-10-06 12:55:20 +0200

    com2

--- lab1/main.js
+++ lab1/main.js
@@ -1,18 +1,24 @@
 const liczba1 = document.querySelector('#liczba1');
-console.dir(liczba1);
-
 const liczba2 = document.querySelector('#liczba2');
-console.dir(liczba2);
-
 const liczba3 = document.querySelector('#liczba3');
-console.dir(liczba3);
-
 const liczba4 = document.querySelector('#liczba4');
-console.dir(liczba4);
+const suma = document.querySelector('#suma');
+const srednia = document.querySelector('#srednia');
+const min = document.querySelector('#min');
+const max = document.querySelector('#max');
+
+
+// console.dir(liczba1);
+// console.dir(liczba2);
+// console.dir(liczba3);
+// console.dir(liczba4);
 
 const przeliczBtn = document.querySelector('#przelicz')
 przeliczBtn.addEventListener('click', () => {
-    console.log(liczba1.value)
+    suma.value = parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)
+    srednia.value = (parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)) / 4
+    min.value = Math.min(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value), parseInt(liczba4.value))
+    max.value = Math.max(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value), parseInt(liczba4.value))
 })
 
 //Math.min(), .max()
