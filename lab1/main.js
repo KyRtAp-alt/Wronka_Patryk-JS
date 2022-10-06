d849f35b1a0d3455c1c35f97e4bdcf0bceaa80d8
KyRtAp-alt <patrykwronka65@wp.pl>
2022-10-06 11:13:52 +0200

    com1

--- lab1/main.js
+++ lab1/main.js
@@ -1,6 +1,15 @@
 const liczba1 = document.querySelector('#liczba1');
 console.dir(liczba1);
 
+const liczba2 = document.querySelector('#liczba2');
+console.dir(liczba2);
+
+const liczba3 = document.querySelector('#liczba3');
+console.dir(liczba3);
+
+const liczba4 = document.querySelector('#liczba4');
+console.dir(liczba4);
+
 const przeliczBtn = document.querySelector('#przelicz')
 przeliczBtn.addEventListener('click', () => {
     console.log(liczba1.value)
