ad46a83a54d250d02ee5fb2e2a086f1704b1c382
KyRtAp-alt <patrykwronka65@wp.pl>
2022-10-13 20:24:32 +0200

    com2

--- Lab2/main.js
+++ Lab2/main.js
@@ -1,24 +1,16 @@
-// notatnik z zajęć
-const main = document.querySelector('main')
-const timeoutRef = setTimeout(
-    () => {
-        main.innerHTML = 'From setTimeout'
-    },
-    2000
-)
-let licznik = 0
-const intervalRef = setInterval(
-    () => {
-        main.innerHTML = 'From interval' + licznik++
-    },
-    4000
-)
+let index = 0;
+showSlides();
 
-// kasujemy setInterval
-clearInterval(intervalRef)
-
-// kasujemy setTimeout
-clearTimeout(intervalRef)
-
-
-// window.requestAnimationFrame
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("img");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    index++;
+    if (index > slides.length) {
+        index = 1
+    }
+    slides[index - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
