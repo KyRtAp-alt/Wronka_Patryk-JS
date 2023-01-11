9017937238a4c13f3d2f3ed7028e1a652bf69ee3
KyRtAp-alt <patrykwronka65@wp.pl>
2023-01-11 17:29:39 +0100

    com2

--- Lab2/main.js
+++ Lab2/main.js
@@ -1,16 +1,56 @@
-let index = 0;
-showSlides();
+// let index = 0;
+// showSlides();
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("img");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("img");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     index++;
+//     if (index > slides.length) {
+//         index = 1
+//     }
+//     slides[index - 1].style.display = "block";
+//     setTimeout(showSlides, 1000);
+// }
+
+let licznik = 0
+
+const main = document.querySelector('main')
+const slides = document.querySelector('.slides')
+
+const intervalRef = setInterval(
+    () => {
+        licznik += 600
+        if (licznik > 1200) {
+            licznik = 0
+        }
+
+        slides.style.left = -licznik + 'px'
+
+        main.innerHTML = 'From interval ' + licznik / 600
+    },
+    1000
+)
+
+document.querySelector('.next').addEventListener('click', () => {
+    licznik = licznik + 600;
+    if (licznik > 1200) {
+        licznik = 0;
     }
-    index++;
-    if (index > slides.length) {
-        index = 1
+
+    slides.style.left = -licznik + 'px';
+})
+
+document.querySelector('.prev').addEventListener('click', () => {
+    licznik = licznik - 600;
+    if (licznik < 0) {
+        licznik = 1200;
     }
-    slides[index - 1].style.display = "block";
-    setTimeout(showSlides, 1000);
-}
+
+    slides.style.left = -licznik + 'px';
+})
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
